6e7f00af716704ec539f9dd1284104acb61fc807
Verta <u3001290@campus.udg.edu>
2024-10-30 11:49:02 +0100

    654

--- exitos/rootfs/server.py
+++ exitos/rootfs/server.py
@@ -3,7 +3,7 @@ import os # Importa el mòdul os
 import sqlDB as db  # Importa la base de dades
 from bottle import Bottle, template, run, static_file, HTTPError, redirect, request # Bottle és el que ens fa de servidor web
 import configparser
-import forecastingModel as forecastingModel
+import forecastingModel # Importa el model de predicció
 
 # Paràmetres de l'execució
 HOSTNAME = '0.0.0.0'
