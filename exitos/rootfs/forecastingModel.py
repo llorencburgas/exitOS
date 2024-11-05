a7c500f5e6dd021f20023b6382663b0805775e9f
u3001290 <151828336+u3001290@users.noreply.github.com>
2024-11-05 12:22:01 +0000

    extracting values from configparser

--- exitos/rootfs/forecastingModel.py
+++ exitos/rootfs/forecastingModel.py
@@ -2,13 +2,43 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import sqlDB as db
 import sqlite3
+import configparser
+import os
 
 class forecastingModel():
-    def __init__(self):
-        '''Constructor de la classe'''
+    def __init__(self, config_path='./user_info.conf'):
+        '''
+        Constructor de la classe
+        '''
 
         self.db = db.sqlDB() # Creem una instància de la base de dades
         self.model = LinearRegression() # Creem una instància del model de regressió lineal
+        self.config_path = config_path # Ruta al ficher de configuració
+
+        # Carreguem les dades de configuració
+        self.config = self.load_user_info_config()
+
+    def load_user_info_config(self):
+        '''
+        Carrega les dades de configuració de l'usuari
+        '''
+        config = configparser.ConfigParser()
+        
+        # Verifica si el fitxer existeix
+        if not os.path.exists(self.config_path):
+            raise FileNotFoundError(f"El fitxer de configuració {self.config_path} no existeix.")
+
+        # Carrega el fitxer de configuració
+        config.read(self.config_path)
+
+        # Extreu les dades de la secció 'UserInfo'
+        return {
+            'asset_id': config.get('UserInfo', 'AssetID', fallback=None),
+            'generator_id': config.get('UserInfo', 'GeneratorID', fallback=None),
+            'source_id': config.get('UserInfo', 'SourceID', fallback=None),
+            'building_consumption_id': config.get('UserInfo', 'BuildingConsumptionID', fallback=None),
+            'building_generation_id': config.get('UserInfo', 'BuildingGenerationID', fallback=None)
+        }
 
     def train_model(self, sensor_id):
         '''
