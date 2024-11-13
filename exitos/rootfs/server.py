2c0d3dd0be3ca8de9ac2e237cd112636944fc755
u3001290 <151828336+u3001290@users.noreply.github.com>
2024-11-13 10:33:00 +0000

    +

--- exitos/rootfs/server.py
+++ exitos/rootfs/server.py
@@ -22,9 +22,9 @@ def serve_static(filepath):
 # Ruta inicial
 @app.get('/')
 def get_init():
-    sensors = database.get_sensor_names_Wh()
+    sensors = ['sensor1', 'sensor2', 'sensor3']
     #ip = request.environ.get('REMOTE_ADDR')
-    return template('./www/main.html', sensors = sensors['entity_id'].tolist()) #, ip = ip
+    return template('./www/main.html', sensors = sensors) #, ip = ip
 
 # Ruta per la configuració de sensors
 @app.get('/configuration')
