f1072ed911cee824f640fe333c9561e1c8e967dd
u3001290 <151828336+u3001290@users.noreply.github.com>
2024-11-29 11:18:16 +0000

    list dir

--- exitos/rootfs/server.py
+++ exitos/rootfs/server.py
@@ -1,9 +1,10 @@
 # server.py és el fitxer principal de l'aplicació web. Aquest fitxer conté la lògica de l'aplicació web i les rutes per a les diferents pàgines HTML.
 import os # Importa el mòdul os
+os.listdir() # Mostra els fitxers del directori actual
 import sqlDB as db  # Importa la base de dades
 from bottle import Bottle, template, run, static_file, HTTPError, redirect, request # Bottle és el que ens fa de servidor web
 #import exitos.rootfs.optimalScheduler.OptimalScheduler as OptimalScheduler
-#from optimalScheduler import ForecastersManager
+#from optimalScheduler.ForecastersManager import ForecastersManager
 
 
 # Paràmetres de l'execució
