d96d12222a2b21b0016a6c51927a3919d21aadfb
siragordillo <u1974750@udg.edu>
2025-03-07 11:35:02 +0100

    Revert "cursor debug"
    This reverts commit f148d5645a5a52feb3f21a70797ad7a10719242a.

--- exitos/rootfs/sqlDB.py
+++ exitos/rootfs/sqlDB.py
@@ -279,6 +279,7 @@ class sqlDB():
                     # Tanca el cursor i confirma els canvis
                     cur.close()
                     self.__con__.commit()
+                    self.__con__.close()    
                     
                     t_ini += timedelta(days=7)
                 
