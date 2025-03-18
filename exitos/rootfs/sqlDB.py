dfbeacf6939e4aa2f02a0cf101efa0d684a8fd31
siragordillo <u1974750@udg.edu>
2025-03-18 10:17:33 +0100

    .

--- exitos/rootfs/sqlDB.py
+++ exitos/rootfs/sqlDB.py
@@ -189,6 +189,7 @@ class sqlDB():
                         self.base_url + "history/period/" + string_start_date +
                         "?end_time=" + string_end_date +
                         "&filter_entity_id=" + sensor_id
+                        + "&minimal_response&no_attributes"
                     )
 
                     sensor_data_historic = pd.DataFrame()
