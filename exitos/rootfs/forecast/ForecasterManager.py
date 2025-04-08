e5ee2d4edf7e23df8e54f4173b0172dfdde5a70a
siragordillo <u1974750@udg.edu>
2025-04-08 12:28:53 +0200

    date

--- exitos/rootfs/forecast/ForecasterManager.py
+++ exitos/rootfs/forecast/ForecasterManager.py
@@ -18,8 +18,8 @@ def obtainmeteoData(latitude, longitude):
     """
     # today = datetime.today().strftime("%Y-%m-%d")
     # tomorrow = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
-    today = "2025-03-17"
-    tomorrow = "2025-03-18"
+    today = "2025-04-03"
+    tomorrow = "2025-04-04"
 
     url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&start_date={today}&end_date={tomorrow}&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation,rain,weathercode,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,et0_fao_evapotranspiration,vapor_pressure_deficit,windspeed_10m,windspeed_100m,winddirection_10m,winddirection_100m,windgusts_10m,shortwave_radiation_instant,direct_radiation_instant,diffuse_radiation_instant,direct_normal_irradiance_instant,terrestrial_radiation_instant"
     response = requests.get(url).json()
