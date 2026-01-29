58411c95fb6c9810aa3c43f422371b3c0aa06e8b
siragordillo <u1974750@udg.edu>
2026-01-29 09:46:32 +0100

    daily task hour

--- exitos/rootfs/server.py
+++ exitos/rootfs/server.py
@@ -1086,7 +1086,7 @@ def config_optimized_devices_HA():
     except Exception as e:
         logger.error(f"❌ [{datetime.now().strftime('%d:%m:%Y %H:%m')}] -  Error configurant horariament un dispositiu a H.A {e}")
 
-schedule.every().day.at("00:00").do(daily_task)
+schedule.every().day.at("23:30").do(daily_task)
 schedule.every().day.at("01:00").do(daily_forecast_task)
 schedule.every().day.at("02:00").do(monthly_task)
 schedule.every().hour.at(":00").do(certificate_hourly_task)
