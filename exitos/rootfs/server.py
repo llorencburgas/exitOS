eb3f8591d2f7d9911137c2d02fdb1b982dacc500
Pol Cañadas Costa <polcanadascosta@gmail.com>
2026-03-10 11:39:36 +0100

    Merge pull request #4
    * Update config.yaml
    
    * Update server.py
    
    * Update server.py
    
    * Add files via upload
    
    * Update server.py
    
    * Update config.yaml
    
    * Update server.py
    
    * Update config.yaml
    
    * Update config.yaml
    
    * Update config.yaml

--- exitos/rootfs/server.py
+++ exitos/rootfs/server.py
@@ -1531,15 +1531,20 @@ def register_llm_tools():
     )
 
 from bottle import ServerAdapter
-from wsgiref.simple_server import make_server, WSGIServer
+from wsgiref.simple_server import make_server, WSGIServer, WSGIRequestHandler
 from socketserver import ThreadingMixIn
+
+class NoLogRequestHandler(WSGIRequestHandler):
+    def log_message(self, format, *args):
+        pass # Silencia els logs HTTP
 
 class ThreadingWSGIServer(ThreadingMixIn, WSGIServer):
     daemon_threads = True
 
 class ThreadedServer(ServerAdapter):
     def run(self, handler):
-        server = make_server(self.host, self.port, handler, server_class=ThreadingWSGIServer)
+        # Silencia els logs HTTP sempre (quiet de Bottle no arriba a l'adaptador)
+        server = make_server(self.host, self.port, handler, server_class=ThreadingWSGIServer, handler_class=NoLogRequestHandler)
         server.serve_forever()
 
 # Funció main que encén el servidor web.
