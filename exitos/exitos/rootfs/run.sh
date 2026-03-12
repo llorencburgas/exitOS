877f16cf84274a74fd39a836f14f8121b1cb8972
Pol Cañadas Costa <polcanadascosta@gmail.com>
2026-03-12 11:29:29 +0100

    Delete exitos/exitos directory

--- exitos/exitos/rootfs/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-echo "Creem la carpeta (/share/exitos/) si no existeix, aqui guardarem fitxers persistents."
-mkdir -p /share/exitos/
-
-echo "Starting server.py..."
-python3 server.py
